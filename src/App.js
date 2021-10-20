59ad53b5dbc5774871350f89b485d30331d13ee9
Begaim Akhmatova <begaimakhmatova91@gmail.com>
2021-10-20 16:27:39 +0600

    completed hw 34

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const [count, setCount] = useState(0);
+
+    const countChangeHundlerPlus = () => {
+        setCount((prevCount) => {
+            return prevCount +1
+        })
+    }
+
+    const countChangeHundlerMinus = () => {
+        setCount((prevCount) => {
+            return  prevCount -1
+        })
+    }
+
+    return (
+        <div className='App'>
+            <p className='sum'>{count}</p>
+            <div onClick={countChangeHundlerPlus}>+</div>
+            <div onClick={countChangeHundlerMinus}>-</div>
+        </div>
+    )
 }
-
 export default App;
